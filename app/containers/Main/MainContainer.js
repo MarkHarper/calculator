1316209bf70ce4c1dc4d1d0eac666feb605a572b
Mark Harper <markharper@Marks-MBP.attlocal.net>
2016-08-11 18:39:40 -0400

    homepage

---
+++ app/containers/Main/MainContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import { container, innerContainer } from './styles.css'
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <div className={container}>
+                <div className={innerContainer}>
+                {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MainContainer;
--- app/index.js
+++ app/index.js
@@ -1,13 +1,5 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import routes from './config/routes.js'
 
-class HelloWorld extends Component {
-  render () {
-    return (
-      <div>
-        {'Hello World!'}
-      </div>
-    )
-  }
-}
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
