ec7407aafd751bb16c95baf38dc88c90e4631391
Mark Harper <markharper20@gmail.com>
2016-08-16 17:45:52 -0400

    redux hooked up

--- app/index.js
+++ app/index.js
@@ -1,5 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import routes from './config/routes.js'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { routerReducer, syncHistoryWithStore } from 'react-router-redux'
+import { hashHistory } from 'react-router'
+import getRoutes from './config/routes.js'
+import * as reducers from 'redux/modules'
 
-ReactDOM.render(routes, document.getElementById('app'));
+const store = createStore(combineReducers({...reducers, routing: routerReducer}), compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
+))
+
+const history = syncHistoryWithStore(hashHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    {getRoutes(() => true, history)}
+  </Provider>,
+document.getElementById('app'))
