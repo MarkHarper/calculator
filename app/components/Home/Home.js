a377ec61a27823c1f5875793762fa917e48ef7df
Mark Harper <markharper20@gmail.com>
2016-08-24 16:07:25 -0400

    change name

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -4,8 +4,8 @@ import { container, title, slogan } from './styles.css'
 export default function Home () {
   return (
     <div className={container}>
-      <p className={title}>{'Life Metrics'}</p>
-      <p className={slogan}>{'Simple apps that get things done.'}</p>
+      <p className={title}>{'Macros'}</p>
+      <p className={slogan}>{'Simple goals that make you fit.'}</p>
     </div>
   )
 }
