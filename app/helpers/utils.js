0da6d9d3988ac1ae590b72e91d2028601f8ecff5
Mark Harper <markharper20@gmail.com>
2016-08-24 15:22:23 -0400

    save checkin to firebase

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -9,7 +9,7 @@ export function formatUserInfo (name, avatar, uid) {
 export function formatCheckin (user, protein, fats, carbs, currentWeight, currentBodyFat) {
   return {
     timestamp: Date.now(),
-    user: user,
+    userId: user.get('uid'),
     protein,
     fats,
     carbs,
