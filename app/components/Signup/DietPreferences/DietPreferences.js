b466fce934938511b519af81fe753b90c6871ad1
Mark Harper <markharper20@gmail.com>
2016-09-10 18:15:38 -0400

    basic signup process complete

--- app/components/Signup/DietPreferences/DietPreferences.js
+++ app/components/Signup/DietPreferences/DietPreferences.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import {formatGoal} from 'helpers/utils'
 
-export default function BasicInfo (props) {
+DietPreferences.propTypes = {
+  navNext: PropTypes.func.isRequired,
+  updateSignupText: PropTypes.func.isRequired,
+  completeSignup: PropTypes.func.isRequired,
+  currentWeight: PropTypes.string.isRequired,
+  targetWeight: PropTypes.string.isRequired,
+  currentBodyFat: PropTypes.string.isRequired,
+  targetBodyFat: PropTypes.string.isRequired,
+  exerciseTime: PropTypes.string.isRequired,
+  exerciseIntensity: PropTypes.string.isRequired,
+  fatPreference: PropTypes.string.isRequired,
+  editableFatPreference: PropTypes.string.isRequired,
+}
+
+export default function DietPreferences (props) {
+  function submitInfo () {
+    let goal = {
+      currentWeight: props.currentWeight,
+      targetWeight: props.targetWeight,
+      currentBodyFat: props.currentBodyFat,
+      targetBodyFat: props.targetBodyFat,
+      exerciseTime: props.exerciseTime,
+      exerciseIntensity: props.exerciseIntensity,
+      fatPreference: props.editableFatPreference,
+    }
+    props.updateGoalForSignup(props.user.get('uid'), goal)
+    props.saveGoalForSignup(props.user, formatGoal(goal))
+    props.completeSignup()
+    props.navNext()
+  }
   return (
     <div>
       <div>
-        <label>{'Fat Preference'}</label>
+        <label>{'Dietary Fat Preference'}</label>
         <input
-          maxLength={5}
-          type='text'/>
+          onChange={(e) => props.updateSignupText('editableFatPreference', e.target.value)}
+          value={props.editableFatPreference}
+          type='text'
+          placeholder={props.fatPreference}/>
       </div>
-      <button> {'Submit and Continue'} </button>
+      <button onClick={submitInfo}> {'Submit'} </button>
     </div>
   )
 }
+
 
--- app/components/Signup/ExerciseInfo/ExerciseInfo.js
+++ app/components/Signup/ExerciseInfo/ExerciseInfo.js
@@ -1,22 +1,56 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import {formatGoal} from 'helpers/utils'
+
+ExerciseInfo.propTypes = {
+  navNext: PropTypes.func.isRequired,
+  updateSignupText: PropTypes.func.isRequired,
+  currentWeight: PropTypes.string.isRequired,
+  targetWeight: PropTypes.string.isRequired,
+  currentBodyFat: PropTypes.string.isRequired,
+  targetBodyFat: PropTypes.string.isRequired,
+  exerciseTime: PropTypes.string.isRequired,
+  exerciseIntensity: PropTypes.string.isRequired,
+  editableExerciseTime: PropTypes.string.isRequired,
+  editableExerciseIntensity: PropTypes.string.isRequired,
+  fatPreference: PropTypes.string.isRequired,
+}
 
 export default function ExerciseInfo (props) {
+  function submitInfo () {
+    let goal = {
+      currentWeight: props.currentWeight,
+      targetWeight: props.targetWeight,
+      currentBodyFat: props.currentBodyFat,
+      targetBodyFat: props.targetBodyFat,
+      exerciseTime: props.editableExerciseTime,
+      exerciseIntensity: props.editableExerciseIntensity,
+      fatPreference: props.fatPreference,
+    }
+    props.updateGoalForSignup(props.user.get('uid'), goal)
+    props.saveGoalForSignup(props.user, formatGoal(goal))
+    props.navNext()
+  }
   return (
     <div>
       <div>
         <label>{'Weekly Exercise Time'}</label>
         <input
-          maxLength={5}
-          type='text'/>
+          onChange={(e) => props.updateSignupText('editableExerciseTime', e.target.value)}
+          value={props.editableExerciseTime}
+          type='text'
+          placeholder={props.exerciseTime}/>
       </div>
       <div>
         <label>{'Exercise Intensity'}</label>
         <input
-          maxLength={5}
-          type='text'/>
+          onChange={(e) => props.updateSignupText('editableExerciseIntensity', e.target.value)}
+          value={props.editableExerciseIntensity}
+          type='text'
+          placeholder={props.exerciseIntensity}/>
       </div>
-      <button> {'Submit and Continue'} </button>
+      <button onClick={submitInfo}> {'Submit and Continue'} </button>
     </div>
   )
 }
+
 
