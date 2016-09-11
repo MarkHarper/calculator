8578bda234b45718d28c79f55b12bbef50a1be1a
Mark Harper <markharper20@gmail.com>
2016-09-11 13:02:01 -0400

    tooltip

--- app/components/GoalDashboard/GoalDashboard.js
+++ app/components/GoalDashboard/GoalDashboard.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react'
 import {AreaChart, Area, XAxis, YAxis,
-  Pie, PieChart} from 'recharts'
+  Pie, PieChart, Tooltip} from 'recharts'
 import {Table} from 'components'
 import {GoalFormContainer} from 'containers'
 import {table, bottomContainer, topContainer} from './styles.css'
@@ -43,7 +43,9 @@ export default function GoalDashboard (props) {
     <div>
       <div className={topContainer}>
         <GoalFormContainer />
-        <AreaChart width={600} height={300} data={mockLineData}>
+        <AreaChart width={600} height={300} data={mockLineData}
+          margin={{top: 10, right: 30, left: 0, bottom: 0}}>
+          <Tooltip/>
           <YAxis />
           <XAxis dataKey='week'/>
           <Area type='monotone' dataKey='weight' stackId="2" stroke='#8884d8' fill='#8884d8' />
